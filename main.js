e810f838f26e33835ccf38f4c07a6ae62fd92b85
oscorelpro <109703969+oscorelpro@users.noreply.github.com>
2022-10-12 19:05:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //define una variable para una imagen nueva
+	img_imgTag.onload = uploadimg; // establece una función para cargar la variable
+	img_imgTag.src = img_image;   // carga la imagen
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+			aplhabetkey();
+			document.getElementById("d1").innerHTML="You pressed Alphabet Key";
+			console.log("alphabet key");
+		}
+	else {
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+	function aplhabetkey()
+
+}
+function numberkey()
+{
+	img_image="Alpkey.png";
+	add();
+}
+function arrowkey()
+{
+}
+function specialkey()
+{
+	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
